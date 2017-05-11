f534e540f9c01daec80e431042a58e9422f524ee
southernwood <wang.weiqing@wustl.edu>
2017-05-11 15:03:22 -0500

    JDBC

---
+++ MusicUI/src/sample/Main.java
@@ -0,0 +1,44 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import sample.model.Datasource;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        controller.listArtists();
+
+        primaryStage.setTitle("Music Database");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        if (!Datasource.getInstance().open()) {
+            System.out.println("FATAL ERROR: Couldn't connect to the database");
+            Platform.exit();
+        }
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        Datasource.getInstance().close();
+    }
+}
